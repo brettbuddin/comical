5b9dc4106d9f8596e7ca00b3942ebd037ad4dfe5
Brett Buddin <brett@intraspirit.net>
2009-08-26 14:25:45 -0400

    Verifying uniqueness of comics. Also added some protection for new comics with no strips.

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,7 +1,7 @@
 class ComicsController < ApplicationController
   def index
     @comics = Comic.find(:all)
-    @updated = @comics.select { |comic| Date.today == comic.strips.find(:last).posted_on }
+    @updated = @comics.select { |comic| Date.today == comic.strips.find(:last).posted_on if comic.strips.find(:last) }
   end
   
   def show
--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,4 +1,5 @@
 class Comic < ActiveRecord::Base
   has_many :strips
   validates_presence_of :name, :feed_url, :fetch_frequency
+  validates_uniqueness_of :name, :feed_url
 end
