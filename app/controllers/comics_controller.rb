013dc1c578e01c3783cfc79e7a128f0edb53a266
Brett Buddin <brett@intraspirit.net>
2009-08-26 15:11:16 -0400

    Added site URL link back and sorted the listing by alpha.

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,10 +1,10 @@
 class ComicsController < ApplicationController
   def index
-    @comics = Comic.find(:all)
+    @comics = Comic.find(:all, :order => 'name ASC')
     @updated = @comics.select { |comic| Date.today == comic.strips.find(:last).posted_on if comic.strips.find(:last) }
   end
   
   def show
-    @comic = Comic.find(params[:id])
+    @comic = Comic.find_by_url_name(params[:id])
   end
 end
--- app/models/comic.rb
+++ app/models/comic.rb
@@ -2,4 +2,8 @@ class Comic < ActiveRecord::Base
   has_many :strips
   validates_presence_of :name, :url_name, :site_url, :feed_url, :fetch_frequency
   validates_uniqueness_of :name, :url_name, :site_url, :feed_url
+  
+  def to_param
+    self.url_name
+  end
 end
