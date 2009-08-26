08683cb9422e486e9fce4513818f153cce34d813
Brett Buddin <brett@intraspirit.net>
2009-08-26 14:40:30 -0400

    Hark, A Vagrant has been added.

---
+++ lib/comic_feed/fetcher/hark_a_vagrant.rb
@@ -0,0 +1,17 @@
+module ComicFeed
+  
+  class HarkAVagrantFetcher < ComicFeed::Fetcher
+    def parse_data(xml)
+      data = {}
+      latest = xml.channel.items[0]
+      if latest
+        data[:description] = latest.title.match(/Hark, a Vagrant: (.*)/)[1].to_s
+        data[:posted_on] = Date.parse(latest.pubDate.to_s)
+        data[:image_url] = latest.description.match(/http:\/\/www.harkavagrant.com\/[^\/]+\/[^\/]+.png/).to_s
+        return data
+      end
+      nil
+    end
+  end
+  
+end
