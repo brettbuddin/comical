650ec7d85a78265028da9344a00b7eda255cd028
Brett Buddin <brett@intraspirit.net>
2009-08-26 14:13:07 -0400

    Realigning some of the views.

--- app/helpers/comics_helper.rb
+++ app/helpers/comics_helper.rb
@@ -2,4 +2,12 @@ module ComicsHelper
   def name_slug(comic)
     "#{comic.name.gsub(/[^a-z0-9]+/i, '-').downcase}"
   end
+  
+  def relative_date(posted_on)
+    if Date.today == posted_on
+      "<time class=\"today\">today</time>"
+    else
+      "<time>#{time_ago_in_words(posted_on)} ago</time>"
+    end
+  end
 end
