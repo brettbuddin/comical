58d3d0416bcd4c8e40b595bfd51879625ab58010
brettbuddin <brett@intraspirit.net>
2009-08-26 20:17:22 -0400

    Put in a task for Cap that will allow for mod_rails restart and changed to use MySQL.

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,3 +37,15 @@ role :db, 'intraspirit.net'
 set :user, 'brett'
 
 set :deploy_to, '/home/brett/web/comical.intraspirit.net/public'
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
