912362d04c0764045db0ee1bd39297fd90dd7ba6
brettbuddin <brett@intraspirit.net>
2009-10-21 22:07:47 -0400

    Making Nedroid's driver take a look at the feed and find the latest that has an embeded comic.

--- lib/comic_feed/fetcher/nedroid_picture_diary.rb
+++ lib/comic_feed/fetcher/nedroid_picture_diary.rb
@@ -4,10 +4,14 @@ module ComicFeed
     def parse_data(xml)
       data = {}
       latest = xml.channel.items[0]
+
+      items = xml.channel.items
+      latest = items.each { |item| break item if item.description.match(/http:\/\/nedroid\.com\/comic\/comics-rss\/[^\/]+\.gif/) }
+
       if latest
         data[:description] = latest.title
         data[:posted_on] = Date.parse(latest.pubDate.to_s)
-        data[:image_url] = latest.description.match(/http:\/\/nedroid\.com\/comics-rss\/[^\/]+\.gif/).to_s
+        data[:image_url] = latest.description.match(/http:\/\/nedroid\.com\/comic\/comics-rss\/[^\/]+\.gif/).to_s
         data[:permalink] = latest.link
 
         return data
