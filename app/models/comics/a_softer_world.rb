2aede937b80e3db844f360d680b3b9b37b4ff077
Brett Buddin <brett@intraspirit.net>
2010-02-23 18:40:55 -0500

    Added A Softer World.

---
+++ app/models/comics/a_softer_world.rb
@@ -0,0 +1,22 @@
+class ASofterWorld < Comic
+  site_name "A Softer World"
+  site_url "http://asofterworld.com"
+  feed_url "http://www.rsspect.com/rss/asw.xml"
+  
+  def map_routine(xml)
+    items = xml.channel.items
+    latest = items.each do |item|
+      break item if item.title.match(/A Softer World: [0-9]+/)
+    end
+
+    if latest
+      real_title = latest.description.match(/title="(.*)"/)[1]
+      number = latest.title.match(/A Softer World: (.*)/)[1]
+      self.description = "#{number}: #{real_title}"
+      self.posted_on = Date.parse(latest.pubDate.to_s)
+      date = self.posted_on.strftime('%Y%m%d')
+      self.permalink = "http://#{site_url.host}/index.php?id=#{number}"
+      self.image_url = latest.description.match(/http:\/\/www\.asofterworld\.com\/clean\/[^\/]+\.jpg/).to_s
+    end
+  end
+end
